89efcb272b3d631e953aa0a31625264bf66b22ec
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2023-05-22 15:18:27 -0500

    Create mario-less.c

---
+++ mario-less.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+void printhash(int height);
+int main(void)
+//Gets input from the user
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+    printhash(height);
+}
+void printhash(int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int h = 0; h <= (height - (i + 2)); h++)
+        {
+            printf(" ");//Prints the spaces
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");//Prints the hashes
+        }
+        printf("\n");//Prints the newline after everyrow
+    }
+
+}
+
