d78e817e619f9558f16b7ec3f0e144d6eee07efa
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2023-09-05 12:37:43 -0500

    Update helpers.c

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,14 +2,14 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0 ; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-       for (int j = 0; j < width; j++)
-       {
-        image[i][j].rgbtBlue = 0x10;
-        image[i][j].rgbtGreen = 0x10;
-        image[i][j].rgbtRed  = 0x20;
-       }
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = 0x10;
+            image[i][j].rgbtGreen = 0x10;
+            image[i][j].rgbtRed = 0x20;
+        }
     }
     // Change all black pixels to a color of your choosing
 }
