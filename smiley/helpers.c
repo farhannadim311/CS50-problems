721b5b6951c003171511ee46e4e235652ab17d16
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2023-09-05 12:33:53 -0500

    Create helpers.c

---
+++ smiley/helpers.c
@@ -0,0 +1,15 @@
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for(int i = 0 ; i < height; i++)
+    {
+       for (int j = 0; j < width; j++)
+       {
+        image[i][j].rgbtBlue = 0x10;
+        image[i][j].rgbtGreen = 0x10;
+        image[i][j].rgbtRed  = 0x20;
+       }
+    }
+    // Change all black pixels to a color of your choosing
+}
